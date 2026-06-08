0599de54112df1ad19993b98a8dcc8c584deb7af
lsim813 <ljubica.simovska@in516ht.com>
2026-06-08 16:17:11 +0200

    Add smoothiefoot api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -34,3 +35,6 @@ if ingredients_list:
         
         session.sql(query=my_insert_stmt).collect()
         st.success(f"Your smoothie is ordered, {name_on_order}!")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
