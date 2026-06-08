5724440211fe4ad961bf3fc331a340abf90772e6
lsim813 <ljubica.simovska@in516ht.com>
2026-06-08 16:51:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 pd_df = my_dataframe.to_pandas()
+st.write(pd_df.columns.tolist())
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", 
